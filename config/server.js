41dbe6146a05caedf04a3889687a8bd4d32f42cb
joaovitors1g <joao_vitor9846@hotmail.com>
2021-05-21 14:14:56 -0300

    feat: add project base structure

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f26a775e53b86218451a85e624741729'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '380e5c49-f114-43e6-8548-5b9b2dde3d29'
+};
